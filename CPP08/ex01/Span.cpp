565f9426a25050915557b7ce36d51115800da446
msosnova <msosnova@student.42.fr>
2022-07-07 11:22:19 +0200

    07.07.22

---
+++ CPP08/ex01/Span.cpp
@@ -0,0 +1,12 @@
+#include <Span.hpp>
+
+Span::Span()
+{
+    _N = 0;
+}
+
+Span::Span(unsigned int N)
+{
+    _N = N;
+
+}
--- CPP08/ex01/Span.hpp
+++ CPP08/ex01/Span.hpp
@@ -1,7 +1,15 @@
 #ifndef SPAN_HPP
 # define SPAN_HPP
 
+# include <iostream>
+# include <vector>
+# include <exception>
+# include <string>
+# include <algorithm>
+
+
 class Span 
+
 {
     private:
         unsigned int        _N;
@@ -23,7 +31,7 @@ class Span
         class maxN: public std::exception
         {
             const char *what() const throw()
-                return "Max size of conteiner is riched";
+                return ("Max size of conteiner is riched");
         };
 
         class noNbr: public std::exception
